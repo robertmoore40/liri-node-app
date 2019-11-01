dd64721d20bf3b39a4373138f675dd17a805786a
Robert Moore <rcmoore3@ncsu.edu>
2019-11-01 12:37:54 -0400

    added functions and switch statement

--- liri.js
+++ liri.js
@@ -1,19 +1,36 @@
 require("dotenv").config();
 
 // 8. Add the code required to import the `keys.js` file and store it in a variable.
-  var keys = require("./keys.js");
 
+var keys = require("./keys.js");
   
 // * You should then be able to access your keys information like so
 
-  var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 // 9. Make it so liri.js can take in one of the following commands:
-
 //    * `concert-this`
-
 //    * `spotify-this-song`
-
 //    * `movie-this`
-
 //    * `do-what-it-says`
+
+var liriInput = process.argv[2];
+switch (liriInput) {
+    case "concert-this":
+        concertThis()
+    break;
+    case "spotify-this-song":
+        spotifyThisSong()
+    break;
+    case "movie-this":
+        movieThis()
+    break;
+    case "do-what-it-says":
+        doWhatItSays()
+    break;
+}
+
+function concertThis() {}
+function spotifyThisSong() {}
+function movieThis() {}
+function doWhatItSays() {}
